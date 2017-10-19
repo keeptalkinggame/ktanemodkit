f2a244efa2a4705e827f21a8937c09a905c9d900
Brian Fetter <brian.fetter.dev@gmail.com>
2017-10-19 13:09:24 -0400

    Added first part of a holdable example.

---
+++ Assets/Examples/1.12 Holdable/BombCreatorExample.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombCreatorExample : MonoBehaviour
+{
+    public TextMesh TimeText;
+    public KMSelectable TimeMinusButton;
+    public KMSelectable TimePlusButton;
+    int time = 300;
+
+    public TextMesh ModulesText;
+    public KMSelectable ModulesMinusButton;
+    public KMSelectable ModulesPlusButton;
+    int modules = 3;
+
+    public KMSelectable StartButton;
+
+    void Start()
+    {
+        UpdateDisplay();
+        TimeMinusButton.OnInteract += delegate () { time -= 30; UpdateDisplay(); return false; };
+        TimePlusButton.OnInteract += delegate () { time += 30; UpdateDisplay(); return false; };
+        ModulesMinusButton.OnInteract += delegate () { modules--; UpdateDisplay(); return false; };
+        ModulesPlusButton.OnInteract += delegate () { modules++; UpdateDisplay(); return false; };
+        StartButton.OnInteract += StartMission;
+    }
+	
+	void Update ()
+    {
+
+    }
+
+    void UpdateDisplay()
+    {
+        TimeText.text = "" + time;
+        ModulesText.text = "" + modules;
+    }
+
+    bool StartMission()
+    {
+        KMGeneratorSetting generatorSettings = new KMGeneratorSetting();
+        generatorSettings.NumStrikes = 3;
+        generatorSettings.TimeLimit = time;
+
+        KMComponentPool pool = new KMComponentPool();
+        pool.SpecialComponentType = KMComponentPool.SpecialComponentTypeEnum.ALL_SOLVABLE;
+        pool.Count = modules;
+        pool.AllowedSources = KMComponentPool.ComponentSource.Mods;
+        generatorSettings.ComponentPools = new List<KMComponentPool> { pool };
+
+        KMMission mission = ScriptableObject.CreateInstance<KMMission>() as KMMission;
+        mission.DisplayName = "Custom Freeplay";
+        mission.GeneratorSetting = generatorSettings;
+
+        GetComponent<KMGameCommands>().StartMission(mission, "" + Random.Range(0, int.MaxValue));
+        return false;
+    }
+}
