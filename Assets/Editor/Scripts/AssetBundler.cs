90f09a7ef37742d87904e772019de250663da45f
Ben Kane <ben@keeptalkinggame.com>
2017-11-16 11:08:31 -0500

    Automatically change preview image asset to be readable and uncompressed for export during asset bundling stage.

--- Assets/Editor/Scripts/AssetBundler.cs
+++ Assets/Editor/Scripts/AssetBundler.cs
@@ -442,8 +442,22 @@ public class AssetBundler
 
         if(ModConfig.PreviewImage != null)
         {
-            byte[] bytes = ModConfig.PreviewImage.EncodeToPNG();
-            File.WriteAllBytes(outputFolder + "/previewImage.png", bytes);
+            string previewImageAssetPath = AssetDatabase.GetAssetPath(ModConfig.PreviewImage);
+
+            if (!string.IsNullOrEmpty(previewImageAssetPath))
+            {
+                TextureImporter importer = AssetImporter.GetAtPath(previewImageAssetPath) as TextureImporter;
+
+                if (!importer.isReadable || importer.textureCompression != TextureImporterCompression.Uncompressed)
+                {
+                    importer.isReadable = true;
+                    importer.textureCompression = TextureImporterCompression.Uncompressed;
+                    importer.SaveAndReimport();
+                }
+
+                byte[] bytes = ModConfig.PreviewImage.EncodeToPNG();
+                File.WriteAllBytes(outputFolder + "/previewImage.png", bytes);
+            }
         }
     }
 
