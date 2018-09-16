405d97fc43109d0a08707986ab2b406fd3ecd385
caitsith2 <d_good@caitsith2.com>
2018-09-15 21:40:04 -0700

    Add Twitch plays support to Button masher module example

--- Assets/Examples/1.8 Button Masher Module/ButtonMasherModule.cs
+++ Assets/Examples/1.8 Button Masher Module/ButtonMasherModule.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using System.Linq;
+using UnityEngine;
 
 /// <summary>
 /// A simple module that requires the player to push the exactly button 50 times, but only
@@ -43,4 +46,41 @@ public class ButtonMasherModule : MonoBehaviour
 
         return false;
     }
+
+	private IEnumerator ProcessTwitchCommand(string command)
+	{
+		var split = command.ToLowerInvariant().Split(new []{" "}, StringSplitOptions.RemoveEmptyEntries);
+		int count;
+
+		if (command == "troll")
+		{
+			yield return "antitroll Sorry, I am not going to waste time showing all numbers from 0 to 200";
+			count = 0;
+			do
+			{
+				Counter.text = count++.ToString();
+				yield return new WaitForSeconds(0.05f * count);
+				Counter.text = currentCount.ToString();
+				yield return "antitroll Aww man. Why you have to stop me? I was at " + count;
+			} while (count < 200);
+		}
+
+		if (split.Length != 3 || !split[0].Equals("mash") || !(new[] {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}).Any(x => x.Equals(split[1])) || !int.TryParse(split[2], out count))
+			yield break;
+
+		while (count > 0)
+		{
+			yield return "trycancel";
+			if (!BombInfo.GetFormattedTime().Contains(split[1])) continue;
+			HandlePress();
+			yield return new WaitForSeconds(0.1f);
+			count--;
+		}
+		
+	}
+
+	private IEnumerator TwitchHandleForcedSolve()
+	{
+		return ProcessTwitchCommand("mash 4 " + (50 - currentCount));
+	}
 }
