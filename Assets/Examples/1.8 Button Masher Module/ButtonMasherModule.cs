56075ae01308af6678f67dc81b10a458282fc50a
caitsith2 <d_good@caitsith2.com>
2018-09-23 19:33:28 -0700

    Quick rotation test implemented.

--- Assets/Examples/1.8 Button Masher Module/ButtonMasherModule.cs
+++ Assets/Examples/1.8 Button Masher Module/ButtonMasherModule.cs
@@ -47,13 +47,60 @@ public class ButtonMasherModule : MonoBehaviour
         return false;
     }
 
+	private float easeInOutQuad(float time, float start, float end, float duration)
+	{
+		time /= duration / 2;
+		if (time < 1)
+			return (end - start) / 2 * time * time + start;
+		time--;
+		return -(end - start) / 2 * (time * (time - 2) - 1) + start;
+	}
+
+	private float getRotateRate(float targetTime, float rate)
+	{
+		return rate * (Time.deltaTime / targetTime);
+	}
+
+	IEnumerator OriginalRotate()
+	{
+		yield return null;
+		//Bomb rotation requires Quaternion.Euler(x,0,0) * Quaternion.Euler(0,y,0) * Quaternion.Euler(0,0,z)	
+		//Camera view rotation requires Quaternion.Euler(x,y,z)	
+		//The new format for this requires returning a Quaterion[] array containing the above two specifcations in that order.	
+		bool frontFace = transform.parent.parent.localEulerAngles.z > 315 || transform.parent.parent.localEulerAngles.z < 45;  //eulerAngles.z = 0 on front face, 180 on back face.	
+		const int angle = 60;
+		for (float i = 0; i <= angle; i += getRotateRate(0.5f, 150))
+		{
+			yield return frontFace
+				? new[] { Quaternion.Euler(easeInOutQuad(i, 0, angle, angle), 0, 0), Quaternion.Euler(easeInOutQuad(i, 0, angle, angle), 0, 0) }
+				: new[] { Quaternion.Euler(easeInOutQuad(i, 0, -angle, angle), 0, 0), Quaternion.Euler(easeInOutQuad(i, 0, -angle, angle), 0, 0) };
+			yield return null;
+		}
+		for (float i = 0; i <= 360; i += getRotateRate(10, 750))
+		{
+			yield return frontFace
+				? new[] { Quaternion.Euler(angle, 0, 0) * Quaternion.Euler(0, easeInOutQuad(i, 0, 360, 360), 0), Quaternion.Euler(angle, easeInOutQuad(i, 0, 360, 360), 0) }
+				: new[] { Quaternion.Euler(-angle, 0, 0) * Quaternion.Euler(0, easeInOutQuad(i, 0, -360, 360), 0), Quaternion.Euler(-angle, easeInOutQuad(i, 0, -360, 360), 0) };
+			yield return null;
+		}
+		for (float i = 0; i <= angle; i += getRotateRate(0.5f, 150))
+		{
+			yield return frontFace
+				? new[] { Quaternion.Euler(easeInOutQuad(i, angle, 0, angle), 0, 0), Quaternion.Euler(easeInOutQuad(i, angle, 0, angle), 0, 0) }
+				: new[] { Quaternion.Euler(easeInOutQuad(i, -angle, 0, angle), 0, 0), Quaternion.Euler(easeInOutQuad(i, -angle, 0, angle), 0, 0) };
+			yield return null;
+		}
+		yield return Quaternion.Euler(0, 0, 0);
+	}
+
 #pragma warning disable 0414
 	private string TwitchHelpMessage = "Solve the module with !{0} mash 4 50.  The first number is the time that has to appear in the time, the second number is the number of times to mash the button";
-#pragma warning restore 0414
+
 #pragma warning disable 0649
 	private bool TwitchShouldCancelCommand;
 	private bool TwitchPlaysSkipTimeAllowed = true;
 #pragma warning restore 0649
+#pragma warning restore 0414
 	private IEnumerator ProcessTwitchCommand(string command)
 	{
 		var split = command.ToLowerInvariant().Split(new []{" "}, StringSplitOptions.RemoveEmptyEntries);
@@ -71,6 +118,14 @@ public class ButtonMasherModule : MonoBehaviour
 		{
 			yield return null;
 			yield return "detonate";
+		}
+
+		if (command == "rotate")
+		{
+			yield return null;
+			IEnumerator rotate = OriginalRotate();
+			while (rotate.MoveNext())
+				yield return rotate.Current;
 		}
 
 		if (command == "troll")
