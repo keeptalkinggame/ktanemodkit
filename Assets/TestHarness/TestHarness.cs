4bf1343e27ce03827ef8e82e537cf67280e9ed7a
Timwi <github@timwi.de>
2019-02-01 00:11:41 +0100

    Remove unused variable

--- Assets/TestHarness/TestHarness.cs
+++ Assets/TestHarness/TestHarness.cs
@@ -462,7 +462,6 @@ public class TestHarness : MonoBehaviour
 	private Vector3 mouseOrigin;    // Position of cursor when mouse dragging starts
 	private bool isPanning;     // Is the camera being panned?
 	private bool isRotating;    // Is the camera being rotated?
-	private bool isZooming;     // Is the camera zooming?
 	private float mouseDownTIme;
 
 	private Transform _camera;
