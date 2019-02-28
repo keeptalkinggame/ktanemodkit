a8dd92cb0c8569222b88ccfa3aaf2f8d0c178d91
Timwi <github@timwi.de>
2019-02-28 17:18:27 +0100

    Add KMBossModule component

---
+++ Assets/Scripts/KMBossModule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KMBossModule : MonoBehaviour
+{
+    public string[] GetIgnoredModules(KMBombModule module, string[] @default = null)
+    {
+        return GetIgnoredModules(module.ModuleDisplayName, @default);
+    }
+
+    public string[] GetIgnoredModules(string moduleDisplayName, string[] @default = null)
+    {
+        if (Application.isEditor)
+            return @default ?? new string[0];
+
+        var bossModuleManagerAPIGameObject = GameObject.Find("BossModuleManager");
+        if (bossModuleManagerAPIGameObject == null) // Boss Module Manager is not installed
+            return @default ?? new string[0];
+
+        var bossModuleManagerAPI = bossModuleManagerAPIGameObject.GetComponent<IDictionary<string, object>>();
+        if (bossModuleManagerAPI == null || !bossModuleManagerAPI.ContainsKey("GetIgnoredModules"))
+            return @default ?? new string[0];
+
+        return ((Func<string, string[]>) bossModuleManagerAPI["GetIgnoredModules"])(moduleDisplayName) ?? @default ?? new string[0];
+    }
+}
