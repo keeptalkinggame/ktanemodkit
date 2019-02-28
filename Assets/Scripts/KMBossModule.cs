b2837c27e86563cc1ca3397b7659a9e2bc0c190f
Timwi <github@timwi.de>
2019-02-28 18:07:33 +0100

    KMBossModule logging

--- Assets/Scripts/KMBossModule.cs
+++ Assets/Scripts/KMBossModule.cs
@@ -16,12 +16,20 @@ public class KMBossModule : MonoBehaviour
 
         var bossModuleManagerAPIGameObject = GameObject.Find("BossModuleManager");
         if (bossModuleManagerAPIGameObject == null) // Boss Module Manager is not installed
+        {
+            Debug.LogFormat(@"[KMBossModule] Boss Module Manager is not installed.");
             return @default ?? new string[0];
+        }
 
         var bossModuleManagerAPI = bossModuleManagerAPIGameObject.GetComponent<IDictionary<string, object>>();
         if (bossModuleManagerAPI == null || !bossModuleManagerAPI.ContainsKey("GetIgnoredModules"))
+        {
+            Debug.LogFormat(@"[KMBossModule] Boss Module Manager does not have a list on record for “{0}”.", moduleDisplayName);
             return @default ?? new string[0];
+        }
 
-        return ((Func<string, string[]>) bossModuleManagerAPI["GetIgnoredModules"])(moduleDisplayName) ?? @default ?? new string[0];
+        var list = ((Func<string, string[]>) bossModuleManagerAPI["GetIgnoredModules"])(moduleDisplayName);
+        Debug.LogFormat(@"[KMBossModule] Boss Module Manager returned list for “{0}”: {1}", moduleDisplayName, list == null ? "<null>" : list.Join(", "));
+        return list ?? @default ?? new string[0];
     }
 }
